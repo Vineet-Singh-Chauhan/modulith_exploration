f0c6d174f38d2044ceb7f9237d8bbf0e1cf33503
Vineet Singh Chauhan <vineetksc@gmail.com>
2026-05-09 02:33:35 +0530

    setting up project

---
+++ src/main/java/com/wiredbarrack/modulith_exploration/ModulithExplorationApplication.java
@@ -0,0 +1,13 @@
+package com.wiredbarrack.modulith_exploration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulithExplorationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulithExplorationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiredbarrack/modulith_exploration/ModulithExplorationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiredbarrack.modulith_exploration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulithExplorationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
